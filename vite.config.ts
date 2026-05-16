e284f4f8109cec213865896c443caed88e15559c
naseaoi <bf5566@qq.com>
2026-05-17 00:29:56 +0800

    v0.5.0

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const scriptOptions: MonkeyOption = {
     userscript: {
         name: "Tieba Remix Dev",
         namespace: "https://github.com/naseaoi/Tieba-Remix",
-        version: "0.4.9",
+        version: "0.5.0",
         description: "贴吧网页端重塑",
         author: "naseaoi",
         license: "MIT",
